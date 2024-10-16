a0dcb722c8c257212ca76583f353bbc891b2f153
E225703E <bertrand.rivard@etu.univ-nantes.fr>
2024-10-16 16:44:08 +0200

    Ajout de la classe Qtree

---
+++ Qtree.java
@@ -0,0 +1,49 @@
+public class Qtree {
+
+    //Attributs
+    private Centre p;
+    private Qtree NO, NE, SE, SO;
+
+    //Constructeur
+    public Qtree(Centre p){
+        this.p = p;
+        NO = null;
+        NE = null;
+        SE = null;
+        SO = null;
+    }
+
+    //Constructeur
+    public Qtree(Centre p, Qtree NO, Qtree NE, Qtree SE, Qtree SO) {
+        this.p = p;
+        this.NO = NO;
+        this.NE = NE;
+        this.SE = SE;
+        this.SO = SO;
+    }
+    
+    //Méthodes
+    //Getter
+
+    public Centre getP() {
+        return p;
+    }
+
+    public Qtree getNO() {
+        return NO;
+    }
+
+    public Qtree getNE() {
+        return NE;
+    }
+
+    public Qtree getSE() {
+        return SE;
+    }
+
+    public Qtree getSO() {
+        return SO;
+    }
+
+    //
+}
