b837cd6e4f8e657201e5e09475841a29874619a2
E225703E <bertrand.rivard@etu.univ-nantes.fr>
2024-10-18 16:05:19 +0200

    Refonte en cours de Qtree

--- src/Qtree.java
+++ src/Qtree.java
@@ -2,29 +2,39 @@ package src;
 public class Qtree {
 
     //Attributs
+    private Plan r; // partie divisé entre les quatres fils
     private Centre p;
     private Qtree NO, NE, SE, SO;
 
-    //Constructeur
-    public Qtree(Centre p){
+    //Constructeur avec seulement un Centre
+    public Qtree(Centre p, Plan r){
+        this.r = r;
         this.p = p;
-        NO = null;
-        NE = null;
-        SE = null;
-        SO = null;
-    }
+        //On va créer le plan de chaque fils en fonction du plan et du centre du Qtree parent
+        //Fils NO
+        NO.r.setUpLeft(r.getUpLeft());
+        NO.r.setUpright(r.getUpright().getX()/2 , r.getUpLeft().getY());
+        NO.r.setDownRight(r.getUpright().getX()/2 , r.getUpLeft().getY()/2);
+        NO.r.setDownLeft(r.getUpLeft().getX(), r.getUpLeft().getY()/2);
 
-    //Constructeur
-    public Qtree(Centre p, Qtree NO, Qtree NE, Qtree SE, Qtree SO) {
-        this.p = p;
-        this.NO = NO;
-        this.NE = NE;
-        this.SE = SE;
-        this.SO = SO;
+
+        //TODO
+        //Fils NE
+        NE = null;
+
+        //Fils SE
+        SE = null;
+
+        //Fils SO
+        SO = null;
     }
     
     //Méthodes
     //Getter
+
+    public Plan getR() {
+        return r;
+    }
 
     public Centre getP() {
         return p;
@@ -45,8 +55,4 @@ public class Qtree {
     public Qtree getSO() {
         return SO;
     }
-
-    /*public Qtree searchQtree(Centre p) {
-
-    }*/
 }
